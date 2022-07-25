429493005f58c8e3be594a06925bc265654b56a0
Bruno Cruz <bfpcruz@gmail.com>
2022-07-25 16:20:10 +0100

    Add single instance model

---
+++ Bonsai.Sleap/PoseCollection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Bonsai.Sleap
+{
+    public class PoseCollection : Collection<Pose>
+    {
+        public PoseCollection()
+        {
+        }
+         
+        public PoseCollection(IList<Pose> list)
+            : base(list)
+        {
+        }
+    }
+
+}
